05d0e4d962b7c2ecfed4fde2c1f4a5ec13d9cdd4
hassansin <mhkr63@gmail.com>
2014-09-02 11:17:09 +0600

    dynamic manifest.webapp

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,4 +4,21 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core');
 	app.route('/').get(core.index);
+	app.route('/manifest.webapp').get(function(req,res){
+		var manifest = {
+		  'name': 'Timelinesync.com',
+		  'description': 'Timelinesync.com',
+		  'launch_path': '/',
+		  'icons': {
+		    '128': '/modules/core/img/brand/log.png'
+		  },
+		  'developer': {
+		    'name': 'hassansin',
+		    'url': 'mailto:rezatxe@gmail.com'
+		  },
+		  'default_locale': 'en',
+		  'fullscreen': true
+		};
+		res.json(manifest);
+	});
 };
